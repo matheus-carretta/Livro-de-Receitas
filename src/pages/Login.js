a724fe75b67ad9113a23e0fe3b79f063b603d113
Thiago Silva <thiago.developer.rj@gmail.com>
2022-03-08 16:18:02 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Login() {
   return (
-    <p>Elemento inicial</p>
+    <form>
+      <label htmlFor="email-input">
+        Email:
+        <input
+          data-testid="email-input"
+          type="text"
+          id="email-input"
+          name="email-input"
+        />
+      </label>
+
+      <label htmlFor="passsword-input">
+        Senha:
+        <input
+          data-testid="passsword-input"
+          type="passsword"
+          id="passsword-input"
+          name="passsword-input"
+        />
+      </label>
+
+      <button
+        data-testid="login-submit-btn"
+        type="submit"
+      >
+        Enter
+      </button>
+    </form>
   );
 }
 
