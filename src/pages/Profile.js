62644c73ad1908205a3844612f1cedfda27a9dcf
Thiago Silva <thiago.developer.rj@gmail.com>
2022-03-10 20:39:54 -0300

    Requisitos 83 e 84

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,20 +3,19 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const getUserEmail = localStorage.getItem('user');
+  const getUserEmailObj = JSON.parse(getUserEmail);
+
+  console.log(getUserEmailObj.email);
+
   return (
     <div>
       <Header title="Profile" isSearch={ false } />
       <p>Tela de perfil</p>
 
-      <label
-        htmlFor="spanEmail"
-        data-testid="profile-email"
-      >
-        Email:
-        <input
-          id="spanEmail"
-        />
-      </label>
+      <span data-testid="profile-email">
+        { getUserEmailObj.email }
+      </span>
 
       <button
         data-testid="profile-done-btn"
