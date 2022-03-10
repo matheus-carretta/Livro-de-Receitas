94e789211e98772500d91ff5bed7c9b9c215c039
Thiago Silva <thiago.developer.rj@gmail.com>
2022-03-10 20:20:34 -0300

    Requisito 82

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,6 +7,38 @@ function Profile() {
     <div>
       <Header title="Profile" isSearch={ false } />
       <p>Tela de perfil</p>
+
+      <label
+        htmlFor="spanEmail"
+        data-testid="profile-email"
+      >
+        Email:
+        <input
+          id="spanEmail"
+        />
+      </label>
+
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Done Recipes
+      </button>
+
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Logout
+      </button>
+
       <Footer />
     </div>
   );
