fb6ba2b89ab895850c2c6800846e956afc852d4e
unknown <matheus.carretta@gmail.com>
2022-03-09 23:50:29 -0300

    merge constants

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,16 +1,10 @@
 export const FETCH_FOODS = 'FETCH_FOODS';
-<<<<<<< HEAD
-export const FETCH_DRINKS = 'FETCH_DRINKS';
-
-export const NUMBER_12 = 12;
-console.log(FETCH_DRINKS);
-=======
 export const FETCH_RECIPE_DETAILS = 'FETCH_RECIPE_DETAILS';
 export const FETCH_RECOMMENDATIONS = 'FETCH_RECOMMENDATIONS';
 
+export const NUMBER_12 = 12;
 export const ZERO = 0;
 export const COPIED_MESSAGE_TIME = 1500;
 export const MIN_INGREDIENTS = 1;
 export const MAX_INGREDIENTS = 20;
 export const MAX_RECOMENDATIONS = 6;
->>>>>>> 0c89cf184771ae703312d82e3a998b495252a9de
