c68ce8b4fcda4c79cb637c006d9825ba70284cfc
Matheus <mattcontassot@gmail.com>
2022-03-14 17:09:52 -0300

    fix - arquivo functions

---
+++ src/services/functions.js
@@ -0,0 +1,42 @@
+export const checkFavorite = (id) => {
+  const getFavoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+  if (getFavoriteRecipes !== null) {
+    return getFavoriteRecipes.find((favorite) => favorite.id === id);
+  }
+  return false;
+};
+
+export const addFavorite = (recipeDetails, type) => {
+  const getFavoriteRecipes = localStorage.getItem('favoriteRecipes');
+
+  const {
+    [`id${type}`]: id,
+    strArea,
+    strCategory,
+    strAlcoholic,
+    [`str${type}`]: name,
+    [`str${type}Thumb`]: image,
+  } = recipeDetails;
+
+  const newFavoriteRecipe = {
+    id,
+    type: type === 'Drink' ? 'drink' : 'food',
+    nationality: type === 'Drink' ? '' : strArea,
+    category: strCategory,
+    alcoholicOrNot: type === 'Meal' ? '' : strAlcoholic,
+    name,
+    image,
+  };
+
+  if (getFavoriteRecipes !== null) {
+    const favoriteRecipes = [...JSON.parse(getFavoriteRecipes), newFavoriteRecipe];
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+  } else localStorage.setItem('favoriteRecipes', JSON.stringify([newFavoriteRecipe]));
+};
+
+export const removeFavorite = (id) => {
+  const getFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const favorites = getFavorites.filter((favorite) => favorite.id !== id);
+  localStorage.setItem('favoriteRecipes', JSON.stringify(favorites));
+};
