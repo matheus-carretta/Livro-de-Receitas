d8480ed69983d9318caa0c0d8b60f78a9c947e65
unknown <matheus.carretta@gmail.com>
2022-03-20 15:59:44 -0300

    conflitos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import drinkIcon from '../images/drinkIcon.svg';
-import exploreIcon from '../images/exploreIcon.svg';
-import mealIcon from '../images/mealIcon.svg';
+import Wine from '../images/wine-glass.png';
+import Soup from '../images/soup.png';
+import Cardapio from '../images/cardapio.png';
 import '../styles/Footer.css';
 
 function Footer() {
   return (
     <footer data-testid="footer">
       <Link to="/drinks">
-        <img src={ drinkIcon } alt="drinkIcon" data-testid="drinks-bottom-btn" />
+        <img
+          src={ Wine }
+          width="40px"
+          height="40px"
+          alt="drinkIcon"
+          data-testid="drinks-bottom-btn"
+        />
       </Link>
       <Link to="/explore">
-        <img src={ exploreIcon } alt="exploreIcon" data-testid="explore-bottom-btn" />
+        <img
+          src={ Cardapio }
+          alt="exploreIcon"
+          width="35px"
+          height="35px"
+          data-testid="explore-bottom-btn"
+        />
       </Link>
       <Link to="/foods">
-        <img src={ mealIcon } alt="mealIcon" data-testid="food-bottom-btn" />
+        <img
+          src={ Soup }
+          width="40px"
+          height="40px"
+          alt="mealIcon"
+          data-testid="food-bottom-btn"
+        />
       </Link>
     </footer>
   );
